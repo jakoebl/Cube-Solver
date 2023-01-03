9ef9d8ea082b6ff4a545659daef98fa15338df3d
jakob <jakob.traber@bluewin.ch>
2023-01-03 12:33:34 +0100

    solver does solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import generate_g1 as g1
 import generate_g2 as g2
-import generate_g3_old as g3
 import generate_g4 as g4
 import move_tables as move
 import facelet_moves as m
@@ -45,13 +44,10 @@ def g2_coordinate(perm):
     return g2.coordinate(result)
 
 
-def g3_coordinate(perm):
+def g3_string(perm):
     result = ""
     for corner in p.corners(perm):
-        if corner in [('u', 'l', 'b'), ('u', 'r', 'f'), ('d', 'l', 'f'), ('d', 'r', 'b')]:
-            result += "0"
-        else:
-            result += "1"
+        result += str(p.corners_solved.index(corner))
     # order found in gen_g3
     transform = [0, 1, 2, 3, 8, 9, 10, 11, 4, 5, 6, 7]
     edges_transformed = []
@@ -61,11 +57,15 @@ def g3_coordinate(perm):
     for edge in edges_transformed:
         if edge in [('u', 'r'), ('u', 'l'), ('d', 'r'), ('d', 'l')]:
             result += "1"
-        elif edge in [('f', 'r'), ('f', 'l'), ('b', 'l'), ('b', 'r')]:
-            result += str([('f', 'r'), ('f', 'l'), ('b', 'l'), ('b', 'r')].index(edge))
-        else:
+        elif edge not in [('f', 'r'), ('f', 'l'), ('b', 'l'), ('b', 'r')]:
             result += "0"
-    return g3.coordinate(result)
+
+    result += "xxxx"
+
+    result += str(p.corner_parity(p.corners(perm)))
+
+    result += "\n"
+    return result
 
 
 def g4_coordinate(perm):
@@ -104,15 +104,17 @@ def string_from_perm(perm):
 # read all lines
 g1_file = open("lookup_g1", "r")
 g2_file = open("lookup_g2", "r")
-g3_file = open("moves_g3", "r")
-g4_move = open("moves_g4", "r")
-g4_string = open("strings_g4", "r")
+moves_g3 = open("moves_g3", "r")
+strings_g3 = open("strings_g3", "r")
+moves_g4 = open("moves_g4", "r")
+strings_g4 = open("strings_g4", "r")
 
 g1_table = g1_file.readlines()
 g2_table = g2_file.readlines()
-g3_table = g3_file.readlines()
-g4_moves = g4_move.readlines()
-g4_strings_temp = g4_string.readlines()
+g3_strings = strings_g3.readlines()
+g3_moves = moves_g3.readlines()
+g4_moves = moves_g4.readlines()
+g4_strings_temp = strings_g4.readlines()
 g4_strings = []
 
 for line in g4_strings_temp:
@@ -120,9 +122,10 @@ for line in g4_strings_temp:
 
 g1_file.close()
 g2_file.close()
-g3_file.close()
-g4_move.close()
-g4_string.close()
+moves_g3.close()
+strings_g3.close()
+moves_g4.close()
+strings_g4.close()
 
 move_list = []
 
@@ -156,25 +159,20 @@ def solve(input_perm):
 
     if g1_coordinate(input_perm) != 0:
         solution_g1 = move.invert(lookup_moves(g1_table, g1_coordinate(input_perm)))
-        if solution_g1:
-            print(solution_g1)
+
         # bring input  to g1
         for move_index in solution_g1:
             input_perm = m.apply_single(input_perm, move.moves[move_index])
 
     if g2_coordinate(input_perm) != 0:
         solution_g2 = move.invert(move.translate(lookup_moves(g2_table, g2_coordinate(input_perm)), move.moves_g2))
-        if solution_g2:
-            print(solution_g2)
 
         # bring input to g2:
         for move_index in solution_g2:
             input_perm = m.apply_single(input_perm, move.moves[move_index])
 
-    if g3_coordinate(input_perm) != 0:
-        solution_g3 = move.invert(move.translate(lookup_moves(g3_table, g3_coordinate(input_perm)), move.moves_g3))
-        if solution_g3:
-            print(solution_g3)
+    if index_string(g3_strings, g3_string(input_perm)) > 95:
+        solution_g3 = move.invert(move.translate(lookup_moves(g3_moves, index_string(g3_strings, g3_string(input_perm))), move.moves_g3))
 
         # bring input to g3:
         for move_index in solution_g3:
@@ -182,8 +180,7 @@ def solve(input_perm):
 
     if input_perm != 'uuuuuuuuffffffffllllllllbbbbbbbbrrrrrrrrdddddddd':
         solution_g4 = move.invert(move.translate(lookup_moves(g4_moves, index_string(g4_strings, string_from_perm(input_perm))), move.moves_g4))
-        if solution_g4:
-            print(solution_g4)
+
         # bring input to g4:
         for move_index in solution_g4:
             input_perm = m.apply_single(input_perm, move.moves[move_index])
@@ -193,4 +190,5 @@ def solve(input_perm):
         return "ERROR"
 
     solution = solution_g1 + solution_g2 + solution_g3 + solution_g4
-    print(move.moves_string(solution))
+
+    return len(solution)
