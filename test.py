185379e3e310685d1390d557d9999ca2f20784cf
jakoebl <120519557+jakoebl@users.noreply.github.com>
2023-01-07 02:58:20 +0100

    cleanup

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import random
 import main
 import move_tables as m
-test_sample = 20000
+test_sample = 10000
 result = 0
 for _ in range(test_sample):
     scramble = m.apply(m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)],
@@ -10,4 +10,7 @@ for _ in range(test_sample):
                        m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)],
                        m.moves[random.randint(0, 17)], m.moves[random.randint(0, 17)])
     result += len(main.solve(scramble))
+    for element in main.solve(scramble):
+        if (element + 1) % 3 == 0:
+            result -= 1
 print(result / test_sample)
